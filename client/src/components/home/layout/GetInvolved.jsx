7559444c91916c3b4228c61f35d352beba97197e
freakmykappachunguslife <edwardzhang858@gmail.com>
2026-01-23 13:37:18 -0800

    Updated GetInvolved.jsx
    Added display for InvolvementCards to GetInvolved section

--- client/src/components/home/layout/GetInvolved.jsx
+++ client/src/components/home/layout/GetInvolved.jsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Text } from "@chakra-ui/react"
+import { Box, Flex, Text, Grid, SimpleGrid, GridItem } from "@chakra-ui/react"
+import InvolvementCard from "../ui/InvolvementCard";
 
 export default function GetInvolved() {
     return (
@@ -15,12 +16,38 @@ export default function GetInvolved() {
                 alignItems="center"
             >
                 <Box
-                    bg="rgb(200, 200, 200)"
+                    bg="#D9D9D9"
                     width="1124px"
                     height="529px"
-                    border="1.5px solid"
+                    border="1px solid"
+                    
                 >
+                <SimpleGrid rows = {2} columns = {3}>
+                    {/* Add links to buttons when applicable*/}
+                    <GridItem colSpan={3} rowSpan = {1}>
+                            <Text align = "center" fontWeight = "bold" fontSize= "5xl">Join Our Mission.</Text>
+                    </GridItem>
+                    
 
+                    <InvolvementCard title="Volunteer"
+                                    description="Lend your time at our events"
+                                    linkname="Volunteer With Us"
+                                    
+                    /> {/* Aligned at 65 pixels right of box left edge*/}
+
+                    <InvolvementCard title="Partner"
+                                    description="Collaborate with us to expand our reach."
+                                    linkname = "Become a Partner"
+                    />
+
+                    <InvolvementCard title="Donate"
+                                    description="Your financial report fuels our mission"
+                                    linkname="Support our Cause"
+                     />
+                     {/*Right edge of button aligned at 89 pixels from box right edge*/}
+
+                </SimpleGrid>
+                    
                 </Box>
             </Flex>
         </Box>
