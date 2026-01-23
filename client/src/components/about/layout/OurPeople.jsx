571ff5fbb1d899c6c4246260cf541d98b59a70a7
Avery Lopez <averyagustin05@gmail.com>
2026-01-22 18:56:51 -0800

    Setup initial structuring for AboutUs Route; Added components for layout, assigned labels

---
+++ client/src/components/about/layout/OurPeople.jsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+export default function OurPeople() {
+    return (
+        <Text>our people - avery</Text>
+    );
+}
---
+++ client/src/components/about/layout/WhoWeAre.jsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+export default function WhoWeAre() {
+    return (
+        <Text>who we are - isaiah</Text>
+    );
+}
