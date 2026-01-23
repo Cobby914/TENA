4817303a20a8d1f5982e848576a2d536ae6f2de2
Niva Phueklieng <nphueklieng@gmail.com>
2026-01-23 09:55:06 -0800

    Add the frame for the Get Involved Section

---
+++ client/src/components/home/layout/GetInvolved.jsx
@@ -0,0 +1,28 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+
+export default function GetInvolved() {
+    return (
+        <Box
+            as="section"
+            bg="rgb(255, 255, 255)"
+            width="100%"
+            height="1000px"
+            p="20"
+        >
+            <Flex
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Box
+                    bg="rgb(200, 200, 200)"
+                    width="1124px"
+                    height="529px"
+                    border="1.5px solid"
+                >
+
+                </Box>
+            </Flex>
+        </Box>
+    );
+}
