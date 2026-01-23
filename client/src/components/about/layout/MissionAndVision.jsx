faf2251ce1fd5738e642d33ebbe93dc20c7cb98b
nigodizon9@gmail.com <nigodizon@mac.lan>
2026-01-23 02:18:40 -0800

    Finished About Us Skeleton Sprint Task

--- client/src/components/about/layout/MissionAndVision.jsx
+++ client/src/components/about/layout/MissionAndVision.jsx
@@ -1,7 +1,66 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Box, Flex, VStack, Button } from "@chakra-ui/react";
+import ImagePlaceholder from "../../ui/MVimagePH";
 
 export default function MissionAndVision() {
     return (
-        <Text>who we are - isaiah</Text>
+        <Box //Section Padding to Center Components
+            as="section"
+            bg="white"
+            width="100%"
+            px="24"
+            py="20"
+        >
+            {/* Centered Page */}
+            <Box maxW="1400px" mx="auto">
+                <VStack spacing="24" alignItems="stretch">
+                    {/* Our Mission + ImagePH */}
+                    <Flex align="center" gap="80">
+                        <Box flex="1">
+                            <Text fontSize="5xl" mb="4">
+                                Our Mission
+                            </Text>
+                            {/* Our Mission Description */}
+                            <Text fontSize="lg" lineHeight="1.35" mb="8">
+                                To eradicate health disparities by empowering underrepresented individuals, building a diverse healthcare workforce, and connecting 
+                                communities to resources, care, and opportunities that promote lifelong wellness
+                            </Text>
+                            {/* Button */}
+                            <Button
+                                bg="rgb(245, 245, 245)"
+                                color="black"
+                                borderRadius="none"
+                                height="60px"
+                                width="260px"
+                                whiteSpace="normal"
+                            >
+                                Get Involved/
+                                <br />
+                                Join the movement
+                            </Button>
+                        </Box>
+                        {/* Image Placeholder */}
+                        <Box flex="1">
+                            <ImagePlaceholder label="IMG" />
+                        </Box>
+                    </Flex>
+
+                    {/* Our Vision + Image PH */}
+                    <Flex allign="center" gap="80px">
+                        <Box flex="1">
+                            <ImagePlaceholder label="IMG" />
+                        </Box>
+                        {/* Header + Text */}
+                        <Box flex="1">
+                            <Text fontSize="5xl" mb="mb">
+                                Our Vision
+                            </Text>
+                            <Text fontSize="lg" lineHeight="1.35">
+                                All people achieving their highest health and wellness
+                            </Text>
+                        </Box>
+                    </Flex>
+                </VStack>
+            </Box>
+        </Box>
     );
 }
--- client/src/components/about/layout/WhoWeAre.jsx
+++ client/src/components/about/layout/WhoWeAre.jsx
@@ -1,7 +1,43 @@
-import { Text } from "@chakra-ui/react";
+import { Text, Box, Flex, VStack, Button } from "@chakra-ui/react";
 
 export default function WhoWeAre() {
     return (
-        <Text>who we are - isaiah</Text>
+        <Box
+            as="section"
+            bg="rgb(184, 184, 184)"
+            width="100%"
+            px="20"
+            py="16"
+        >
+            <Flex direction="column" alignItems="flex=start" maxW="950px">
+                <Text fontSize="6xl" fontWeight="normal" mb="6">
+                    Who We Are
+                </Text>
+                {/* Paragraph */}
+                <VStack spacing="6" alignItems="flex-start" mb="10">
+                    <Text fontSize="lg" lineHeigh="1.35">
+                        TENA is a community-centric nonprofit building ecosystems that empower underserved communities to overcome systemic barriers 
+                        through optimizing access to resources, knowledge, and entry to healthcare professions.
+                    </Text>
+                    <Text fontSize="lg" lineHeight="1.35">
+                        TENA recognizes that access to quality healthcare, resources, and health education can be difficult to obtain in many
+                        communities, and works hard to bridge this gap. Composed primarily of Los Angeles County natives, the team draws upon the 
+                        social determinants of health to provide services that support the health and wellness of their neighborhoods and beyond. 
+                        TENA believes in empowering others to have the tools, knowledge, and resources they need to live a healthy and enriched life.
+                    </Text>
+                </VStack>
+                {/* Find Out More */}
+                <Button
+                    bg="white"
+                    color="black"
+                    fontWeight="normal"
+                    borderRadius="none"
+                    height="70px"
+                    width="220px"
+                >
+                    Find Out More
+                </Button>
+            </Flex>
+        </Box>
     );
 }
