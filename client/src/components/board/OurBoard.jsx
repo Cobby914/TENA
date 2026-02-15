52aef63dfd0d45f3ec61378090f717b6800d38d1
Niva Phueklieng <nphueklieng@gmail.com>
2026-02-15 15:26:04 -0800

    Route the button in 'About Us' page to the Board Page

---
+++ client/src/components/board/OurBoard.jsx
@@ -0,0 +1,11 @@
+import { Box, Text, SimpleGrid } from "@chakra-ui/react";
+
+export default function OurBoard() {
+    return (
+        <Box>
+            <Text font>
+                Our Board
+            </Text>
+        </Box>
+    );
+}
---
+++ client/src/pages/Board.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/home/layout/Navbar";
+import Footer from "../components/home/layout/Footer";
+import OurBoard from "../components/board/OurBoard";
+
+export default function Board() {
+    return (
+        <>
+            <Navbar/>
+            <OurBoard/>
+        </>
+    );
+}
