b3f0c2a7e4ee2a25cc139b5a7753acd2f811c069
Niva Phueklieng <nphueklieng@gmail.com>
2026-02-15 16:26:11 -0800

    Implement Board Page from Figma Design

--- client/src/components/board/OurBoard.jsx
+++ client/src/components/board/OurBoard.jsx
@@ -1,11 +1,36 @@
 import { Box, Text, SimpleGrid } from "@chakra-ui/react";
+import MemberCard from "../ui/MemberCard";
 
 export default function OurBoard() {
     return (
-        <Box>
-            <Text font>
+        <Box p={20}>
+            <Text fontSize={40}>
                 Our Board
             </Text>
+            {/* 4 by 4 Cards of Board Members */}
+            <SimpleGrid mt={10} columns={4} spacing={10}>
+
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+                <MemberCard name="Jemal Hussein" position="Position in Organization" />
+
+            </SimpleGrid>
         </Box>
     );
 }
