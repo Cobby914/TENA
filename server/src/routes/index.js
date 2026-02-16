c8d4fb9d021e383e60efd0aa067aa4d29829cb20
Avery Lopez <averyagustin05@gmail.com>
2026-02-16 00:36:48 -0800

    Fixed routes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,7 +5,7 @@ import teamMembersRouter from "./team_members.js";
 import newsletterSubscribersRouter from "./newsletter_subscribers.js";
 import companyInfoRouter from "./company_info.js";
 import programStatsRouter from "./program_stats.js";
-import users from "./users.js";
+import usersRouter from "./users.js";
 
 const router = Router();
 
@@ -15,8 +15,8 @@ router.use("/newsletter-subscribers", newsletterSubscribersRouter);
 router.use("/team_members", teamMembersRouter);
 router.use("/newsletter_subscribers", newsletterSubscribersRouter);
 router.use("/company_info", companyInfoRouter);
-router.use("/program_stats", companyInfoRouter);
-router.use("/users", companyInfoRouter);
+router.use("/program_stats", programStatsRouter);
+router.use("/users", usersRouter);
 
 // router.use("/auth", authRoutes);
 // router.use("/programs", programsRoutes);
