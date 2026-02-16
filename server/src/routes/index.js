ac8ad7618d9951ede99d4fafe3fe1502a68d066f
nigodizon9@gmail.com <nigodizon@mac.lan>
2026-02-15 19:39:04 -0800

    Finished CRUD Route: programs, team_members, newsletter_subscirbers

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,18 @@
 import { Router } from "express";
 
+import programsRouter from "./programs.js";
+import teamMembersRouter from "./team_members.js";
+import newsletterSubscribersRouter from "./newsletter_subscribers.js";
+//Ave put your routes here:
 
 const router = Router();
+
+router.use("/programs", programsRouter);
+router.use("/team-members", teamMembersRouter);
+router.use("/newsletter-subscribers", newsletterSubscribersRouter);
+router.use("/team_members", teamMembersRouter);
+router.use("/newsletter_subscribers", newsletterSubscribersRouter);
+//Ave put 'router.use' here:
 
 // router.use("/auth", authRoutes);
 // router.use("/programs", programsRoutes);
