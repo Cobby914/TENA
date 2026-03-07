c8b887ee457a0166cba646328d2ed50696422e6d
Edward <edwardzhang858@gmail.com>
2026-03-06 18:12:35 -0800

    All pages snap to the top of the page on change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import {useEffect} from "react";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import ReusableProgram from "./pages/ReusableProgram";
@@ -10,9 +11,21 @@ import Admin from "./pages/Admin";
 import Partners from "./pages/Partners";
 import Approvals from "./pages/Approvals";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+
 export default function App() {
     return (
         <BrowserRouter>
+        <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<AboutUs />} />
