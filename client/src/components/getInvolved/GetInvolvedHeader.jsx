19fabb86acb64d1ca85eedfcdd967d42ed56ba32
Edward <edwardzhang858@gmail.com>
2026-03-28 03:58:46 -0700

    Early commit for CK

--- client/src/components/getInvolved/GetInvolvedHeader.jsx
+++ client/src/components/getInvolved/GetInvolvedHeader.jsx
@@ -4,8 +4,11 @@ export default function GetInvolvedHeader() {
     return(
         <Box 
             as="section"
-            bg="white"
+            bg="#092751"
             width="100%"
+            align = "center"
+            gap = "40px"
+            height = {{base: "200px", md: "450px"}}
             pt={{ base: 10, md: 16, lg: 20 }}
             pb={{ base: 6, md: 8, lg: 10 }}
         >
@@ -14,13 +17,15 @@ export default function GetInvolvedHeader() {
                 mx="auto"
                 px={{ base: 4, md: 10, lg: 20 }}
             >
-                <VStack spacing={{ base: 4, md: 6 }} textAlign="center" maxW="900px" mx="auto">
-                    <Text fontSize={{ base: "3xl", md: "5xl", lg: "7xl" }} fontWeight="normal" lineHeight="1.05">
-                        Help us out. Get Involved.
+                <VStack spacing={{ base: 4, md: 6 }} textAlign="center" maxW="1000px" mx="auto">
+                    <Text fontFamily ="INTER" fontSize ={{base: "36px", md : "72px"}} fontStyle = "normal" fontWeight = "800" lineHeight="100%" color = "#1573CF">
+                        Help us 
+                        <Text as = "span" fontFamily ="INTER" fontSize ={{base: "36px", md : "72px"}} fontStyle = "normal" fontWeight = "800" lineHeight="100%" color = "#5CDAC5"> Out</Text>
+                        . Get 
+                        <Text as = "span" fontFamily ="INTER" fontSize ={{base: "36px", md : "72px"}} fontStyle = "normal" fontWeight = "800" lineHeight="100%" color = "#5CDAC5"> Involved</Text>
                     </Text>
-                    <Text fontSize={{ base: "md", md: "lg" }} lineHeight="1.6">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur..
-                    </Text>
+                    <Text color = "#F8F9FB" maxW = "982px" fontFamilly = "INTER" fontSize = {{base: "12px", md : "24px"}} fontStyle = "normal" fontWeight = "400" lineHeight="150%">
+TENA’s work is powered by community, partnership, and shared purpose. Whether you want to give your time, support our mission, collaborate with us, or simply find help, here’s how you can be part of lasting change.                    </Text>
                 </VStack>
             </Box>
         </Box>
