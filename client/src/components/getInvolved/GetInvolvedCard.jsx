ff0b745a5b0488622ca6b690620c5ca1fc5d9f88
Edward <edwardzhang858@gmail.com>
2026-03-28 22:23:05 -0700

    Removed potentially buggy code

--- client/src/components/getInvolved/GetInvolvedCard.jsx
+++ client/src/components/getInvolved/GetInvolvedCard.jsx
@@ -39,10 +39,10 @@ export default function GetInvolvedCard ({
             
                 
                 <VStack alignItems="flex-start" spacing={{ base: 3, md: 4 }} width="100%" ml = {{base: "30px",md : "50px"}}>
-                    <Text fontFamily = {"Inter"} fontWeight = "700" fontStyle = "normal" fontSize = {{base: "18px", md : "36px"}} lineHeight= "120%">
+                    <Text fontWeight = "700" fontStyle = "normal" fontSize = {{base: "18px", md : "36px"}} lineHeight= "120%">
                         {title}
                     </Text>
-                    <Text fontFamily = "Inter" fontWeight = "400" fontStyle = "normal" fontSize = {{base: "12px", md : "24px"}} lineHeight= "150%"
+                    <Text  fontWeight = "400" fontStyle = "normal" fontSize = {{base: "12px", md : "24px"}} lineHeight= "150%"
                         maxW = "539px">
                         {description}
                     </Text>
@@ -60,7 +60,8 @@ export default function GetInvolvedCard ({
                                 px={{ base: 8, md: 10 }}
                                 rightIcon={<Icon as={ChevronRight} boxSize={{ base: "12px", lg: "18px"}}/>}
                                 _hover = {{bg: "#3f5f85"}}
-                                fontFamily = {"INTER"}
+                                    
+                                zIndex = "2"
                             >
                                 {buttonName}
                             </Button>
