c98ef704a41ca02eec8f5523399125c35eac5d3d
Cobby914 <colinkwon914@gmail.com>
2026-03-30 02:46:39 -0700

    Made MissionAndVision larger

--- client/src/components/about/layout/MissionAndVision.jsx
+++ client/src/components/about/layout/MissionAndVision.jsx
@@ -17,28 +17,28 @@ export default function MissionAndVision() {
             zIndex = "1"
         >
             <Box
-                maxW="1179px"
+                maxW="1320px"
                 mx="auto"
                 px={{ base: 4, md: 10, lg: 24 }}
                 position="relative"
                 zIndex = "3"
             >
                 <VStack spacing={{ base: 8, md: 12, lg: 16 }} alignItems="stretch">
-                    <Flex direction={{ base: "column", lg: "row" }} align="center" gap={{ base: 4, md: 6, lg: 10 }}>
-                        <Box flex="1" maxW={{ lg: "600px" }} width="100%" align = "right">
-                            <Text fontSize={{ base: "18px", md: "36px" }} mb={{ base: 3, md: 4 }} fontWeight = "700" color = "#1573CF">
+                    <Flex direction={{ base: "column", lg: "row" }} align="center" gap={{ base: 6, md: 8, lg: 12 }}>
+                        <Box flex="1" maxW={{ lg: "700px" }} width="100%" align = "right">
+                            <Text fontSize={{ base: "22px", md: "42px" }} mb={{ base: 3, md: 4 }} fontWeight = "700" color = "#1573CF">
                                 Our Goal
                             </Text>
-                            <Text fontSize={{ base: "10px", md: "20px" }} lineHeight="1.35" mb={{ base: 6, md: 8 }} fontWeight = "400" color = "#3F5F85">
+                            <Text fontSize={{ base: "13px", md: "22px" }} lineHeight="1.4" mb={{ base: 6, md: 8 }} fontWeight = "400" color = "#3F5F85">
 Today, TENA continues to grow as a community-anchored nonprofit tackling inequities through programs that are responsive, evidence-informed, and rooted in human dignity.                            </Text>
                             <NavLink to="/getInvolved">
                             <Button
                                 bg="#1573CF"
                                 color=""
                                 borderRadius="none"
-                                height={{ base: "55px", md: "60px" }}
-                                width={{ base: "160px", md: "185px"}}
-                                fontSize={{ base: "sm", md: "md" }}
+                                height={{ base: "58px", md: "66px" }}
+                                width={{ base: "170px", md: "210px"}}
+                                fontSize={{ base: "md", md: "lg" }}
                                 whiteSpace="normal"
                                 rounded = {{base: "3", md: "6"}}
                                 
@@ -49,7 +49,7 @@ Today, TENA continues to grow as a community-anchored nonprofit tackling inequit
                             </NavLink>
 
                         </Box>
-                        <Box flex="1" width="100%" position="relative">
+                        <Box flex="1" width="100%" maxW={{ lg: "700px" }} position="relative">
                             <Box
                                 position="absolute"
                                 top="-50px"
@@ -65,19 +65,19 @@ Today, TENA continues to grow as a community-anchored nonprofit tackling inequit
                                     opacity={0.2}
                                 />
                             </Box>
-                            <Image src={OurVision} position="relative" zIndex="1" />
+                            <Image src={OurVision} position="relative" zIndex="1" width="100%" objectFit="cover" />
                         </Box>
                     </Flex>
 
-                    <Flex direction={{ base: "column-reverse", lg: "row" }} align="center" gap={{ base: 4, md: 6, lg: 10 }}>
-                        <Box flex="1" width="100%" maxW = "1000px" maxH = "324px">
-                            <Image src = {OurMission} objectFit="contain"/>
+                    <Flex direction={{ base: "column-reverse", lg: "row" }} align="center" gap={{ base: 6, md: 8, lg: 12 }}>
+                        <Box flex="1" width="100%" maxW = "1100px" maxH = "380px">
+                            <Image src = {OurMission} width="100%" objectFit="contain"/>
                         </Box>
-                        <Box flex="1" maxW= "600px" width="100%" maxH = "324px">
-                            <Text fontSize={{ base: "18px", md: "36px" }} mb={{ base: 1, md: 2 }} fontWeight = "700" color = "#1573CF">
+                        <Box flex="1" maxW= "700px" width="100%" maxH = "380px">
+                            <Text fontSize={{ base: "22px", md: "42px" }} mb={{ base: 1, md: 2 }} fontWeight = "700" color = "#1573CF">
                                 Our Mission
                             </Text>
-                            <Text fontSize={{ base: "10px", md: "20px" }} lineHeight="150%" mb={{ base: 2, md: 4 }} fontStyle = "normal"  fontWeight = "400" color = "#3F5F85">
+                            <Text fontSize={{ base: "13px", md: "22px" }} lineHeight="1.5" mb={{ base: 2, md: 4 }} fontStyle = "normal"  fontWeight = "400" color = "#3F5F85">
                                 To eradicate health disparities by empowering underrepresented individuals, building a diverse healthcare workforce, and connecting communities to resources, care, and opportunities that promote lifelong wellness.
                             </Text>
                             <NavLink to="/home">
@@ -85,9 +85,9 @@ Today, TENA continues to grow as a community-anchored nonprofit tackling inequit
                                     bg="#1573CF"
                                     color=""
                                     borderRadius="none"
-                                    height={{ base: "55px", md: "60px" }}
-                                    width={{ base: "160px", md: "185px" }}
-                                    fontSize={{ base: "sm", md: "md" }}
+                                    height={{ base: "58px", md: "66px" }}
+                                    width={{ base: "170px", md: "210px" }}
+                                    fontSize={{ base: "md", md: "lg" }}
                                     whiteSpace="normal"
                                     rounded = {{base: "3", md: "6"}}
                                     fontWeight = "600"
