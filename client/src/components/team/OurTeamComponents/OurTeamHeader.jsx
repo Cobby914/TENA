15861c58cc2eaca14d6286c7408c2de138abebe6
Cobby914 <colinkwon914@gmail.com>
2026-03-30 04:35:27 -0700

    Updated the header for all of about sub pages

--- client/src/components/team/OurTeamComponents/OurTeamHeader.jsx
+++ client/src/components/team/OurTeamComponents/OurTeamHeader.jsx
@@ -1,14 +1,41 @@
 import { Box, Text } from "@chakra-ui/react";
+import MultiRingCircle from "../../ui/MultiRingCircle";
+
+const RING_COLOR = "#092751";
+const RING_SIZE_PX = 487;
 
 export default function OurTeamHeader() {
   return (
     <Box
       as="section"
+      position="relative"
       width="100%"
-      bg="#092751"
+      bg="#3F5F85"
       minH={{ base: "280px", md: "360px", lg: "415px" }}
+      overflow="hidden"
     >
       <Box
+        position="absolute"
+        right={{ base: "32px", md: "72px", lg: "120px" }}
+        bottom={0}
+        w={`${RING_SIZE_PX}px`}
+        h={`${RING_SIZE_PX}px`}
+        opacity={0.54}
+        transform="translate(50%, 50%)"
+        pointerEvents="none"
+        aria-hidden
+      >
+        <MultiRingCircle
+          width={RING_SIZE_PX}
+          height={RING_SIZE_PX}
+          variant="solid"
+          solidColor={RING_COLOR}
+        />
+      </Box>
+
+      <Box
+        position="relative"
+        zIndex={1}
         maxW="1536px"
         mx="auto"
         px={{ base: 6, md: 12, lg: 20 }}
