8ce2d159e0961ca1394b0d7e560e2fb03a695316
Niva Phueklieng <nphueklieng@gmail.com>
2026-03-31 16:29:31 -0700

    Fix footer spacing and stuff also the stretching issue when zoomed out

--- client/src/components/shell/Footer.jsx
+++ client/src/components/shell/Footer.jsx
@@ -15,91 +15,75 @@ export default function Footer({
  children, ...props}
 ) {
     return (
-        <Box as="section"
-             bg="#FFFFFF"
-             width="100%"
-             minH="100%"
-            >
-        
-            <Flex direction="column"
-                justifyContent="center" 
-                alignItems = "center"
-                >
-
-                <Box background="rgba(248, 249, 251)" width="100%" ratio={1537/613}  py={{ base: 10, md: 12 }} px={{ base: 6, md: 8, lg: 10 }}>
-                {/* For heights and widths smaller than the max, just use the same as a ratio of our maxes */}
-        
-                    <Stack direction={{base: "column", lg: "row"}} width="100%" h="100%" gap={{ base: 12, lg: 10 }} alignItems="flex-start">    
-                        <VStack align="flex-start" width={{base: "100%", lg:"65%"}} gap={6}>
-
-                            {/* Logos go here */}
-                            <HStack gap={0}>
-                                <Image src = {main_logo} align = "left"  ratio= {85/81} width={{ base: "60px", lg: "85px" }}
-                                maxWidth = "85px"/>
-                            
-                                {/* This will be the logo text in future, so ratio/size will still be TBD */}
-                                <Image src = {text_logo} align = "right" ratio={153/51} width={{base: "150px", lg: "200px"}}
-                                maxWidth = "200px" />
-                            </HStack>
-
-                            {/* Separator Line */}
-
-                            <Box width="100%"  height={{base: "2px", lg: "4px"}} background = "rgba(92, 218, 197, 1)" />
-
-                            <HStack gap={{ base: "10px", md: "65px" }} alignItems="flex-start" width="100%">
-                                <VStack  minW="200px" gap={10} alignItems="flex-start" >
-                                    <Box>
-                                        <Text fontSize={{ base: 12, md: 14, lg: 16}} fontWeight={500} lineHeight="tall">1001 E Rosecrans Ave</Text>
-                                        <Text fontSize={{ base: 12, md: 14, lg: 16}} fontWeight={500} lineHeight="tall">Los Angeles, CA 90059</Text>
-                                        <Text fontSize={{ base: 12, md: 14, lg: 16}} fontWeight={500} lineHeight="tall">United States</Text>
-                                    </Box>
-                                    <HStack gap={4}>
-                                        <Link href="https://www.linkedin.com/company/tena-cares/" isExternal>
-                                            <Icon as={FaLinkedin} boxSize={7} />
-                                        </Link>
-                                        <Link href="https://www.instagram.com/tenacares/" isExternal>
-                                            <Icon as={FaInstagram} boxSize={7} />
-                                        </Link>
-                                    </HStack>
-                                </VStack>
-                                <VStack minW="150px" gap={6} alignItems="flex-start">
-                                    <NavLink to="/about">
-                                        <Text fontSize={{ base: 20, md: 22, lg: 24}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>About Us</Text>
-                                    </NavLink>
-                                    <NavLink to="/programs">
-                                        <Text fontSize={{ base: 20, md: 22, lg: 24}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>Programs</Text>
-                                    </NavLink>
-                                    <NavLink to="/getInvolved">
-                                    <Text fontSize={{ base: 20, md: 22, lg: 24}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>Get Involved</Text>
-                                    </NavLink>
-                                    <NavLink to="/donate">
-                                        <Text fontSize={{ base: 20, md: 22, lg: 24}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>Donate</Text>
-                                    </NavLink>
-                                </VStack>
-
-                                <VStack maxW="260px" gap={5} alignItems={"flex-start"}>
-                                    <Text fontSize={{ base: 12, md: 14, lg: 16}} fontWeight={500} lineHeight="24px" width="100%">TENA is a 501(c)(3) not-for-profit organization.</Text>
-                                    <Text fontSize={{ base: 12, md: 14, lg: 16}} fontWeight={500} lineHeight="24px" width="100%">Our Tax ID: 83-4267004</Text>
-                                </VStack>
-
-                            </HStack>
-
-                            <HStack mt={6} align="flex-end" justify="space-between" width="100%">
-                                <Image src={gold_transparency} ratio={1} width={"90px"} />
-                                <Text fontSize={{ base: 12, md: 14, lg: 16}} fontWeight={500} lineHeight="24px" >© 2026 TENA CARE. All rights reserved</Text>
-                            </HStack>
-                        </VStack>
-                    
-                        <Box align="center" w={{base: "100%", lg:"40%"}} mt={{base: 10, lg: 0}} ml={{base: 0, lg: 14}}>
-                            <Newsletter></Newsletter>
-                        </Box>
-                    
-                    </Stack>
-                </Box>
-
-            </Flex>
+        <Box width="100%" overflow="hidden" position="relative">
+            <Box as="footer" width="100vw" maxH="610px" background="rgba(248, 249, 251)" border="1px solid rgba(0, 0, 0, 0.17)" py={{ base: 1, md: 2 }} px={{ base: 8, md: 14 }}>
+                <Flex direction={{ base: "column", lg: "row" }} justify={"center"} align={"center"} gap={20}>
+                    <VStack width="900px" height="600px" py={16}>
             
-        </Box>
+                            <VStack align="flex-start" width="100%" gap={4}>
+                                {/* Logos go here */}
+                                <HStack gap={0} align="center" justify="center">
+                                    <Image src = {main_logo} align = "left" ratio= {85/81} width="85px" />
+                                    {/* This will be the logo text in future, so ratio/size will still be TBD */}
+                                    <Image src = {text_logo} align = "right" ratio={153/51} width="180px" ml={-4}/>
+                                </HStack>
 
+                                {/* Separator Line */}
+                                <Box width="800px"  height="2px" background = "rgba(92, 218, 197, 1)" my={2} />
+
+                                <HStack pt={6} alignItems="flex-start" width="890px" justify="space-between">
+                                    <VStack  minW="200px" gap={8} alignItems="flex-start" >
+                                        <Text fontSize={{ base: 12, md: 14, lg: 15}} fontWeight={500} lineHeight="20px" letterSpacing={0}>
+                                            1001 E Rosecrans Ave<br/>Los Angeles, CA 90059<br/>United States
+                                        </Text>
+                                        
+                                        <HStack gap={4}>
+                                            <Link href="https://www.linkedin.com/company/tena-cares/" isExternal>
+                                                <Icon as={FaLinkedin} boxSize={7} />
+                                            </Link>
+                                            <Link href="https://www.instagram.com/tenacares/" isExternal>
+                                                <Icon as={FaInstagram} boxSize={7} />
+                                            </Link>
+                                        </HStack>
+                                    </VStack>
+
+                                    <VStack minW="150px" gap={6} px={6} alignItems="flex-start">
+                                        <NavLink to="/about">
+                                            <Text fontSize={{ base: 20, md: 22}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>About Us</Text>
+                                        </NavLink>
+                                        <NavLink to="/programs">
+                                            <Text fontSize={{ base: 20, md: 22}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>Programs</Text>
+                                        </NavLink>
+                                        <NavLink to="/getInvolved">
+                                        <Text fontSize={{ base: 20, md: 22}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>Get Involved</Text>
+                                        </NavLink>
+                                        <NavLink to="/donate">
+                                            <Text fontSize={{ base: 20, md: 22}} fontWeight={700} _hover={{ color: "rgba(92, 218, 197, 1)" }}>Donate</Text>
+                                        </NavLink>
+                                    </VStack>
+
+                                    <VStack maxW="260px" gap={5} alignItems={"flex-start"}>
+                                        <Text fontSize={{ base: 12, md: 14, lg: 15}} fontWeight={500} lineHeight="20px" width="100%">
+                                            TENA is a 501(c)(3) not-for-profit organization.<br/><br/>Our Tax ID: 83-4267004
+                                        </Text>
+                                    </VStack>
+
+                                </HStack>
+
+                                <HStack mt={6} align="flex-end" justify="space-between" width="100%">
+                                    <Image src={gold_transparency} ratio={1} width={"90px"} />
+                                    <Box px={40}>
+                                        <Text fontSize={{ base: 12, md: 14, lg: 15}} fontWeight={500} lineHeight="24px" ml={-10}>© 2026 TENA CARE. All rights reserved</Text>
+                                    </Box>
+                                </HStack>
+                            </VStack>
+                    </VStack>
+
+                    <Box align="center" w="450px" h="500px" >
+                        <Newsletter></Newsletter>
+                    </Box>
+                </Flex>
+            </Box>
+        </Box>
     );
 }
