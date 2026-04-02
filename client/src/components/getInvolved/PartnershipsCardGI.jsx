b776ad039891fa2a7b9a35b4f909e9d401474e99
Avery Lopez <averyagustin05@gmail.com>
2026-04-01 20:44:49 -0700

    Finished removing vectors, used simple circle and multiring circle component classes instead of images

--- client/src/components/getInvolved/PartnershipsCardGI.jsx
+++ client/src/components/getInvolved/PartnershipsCardGI.jsx
@@ -1,55 +1,59 @@
-import { Box, Button, Flex, HStack, Text, VStack, Card, Image } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
+import SimpleCircle from "../ui/SimpleCircle";
 
 const Partners = "/GetInvolved/GetInvolvedPartnerships.jpg";
-const PTop = "/GetInvolved/vectors/PartnershipTop.svg";
-const PBottom = "/GetInvolved/vectors/PartnershipBottom.svg";
 import GIC from "./GetInvolvedCard"
 
-export default function GetInvolvedCard ({
-
-}) {
+export default function GetInvolvedCard ({}) {
     return (
-    <Box width="100%" overflowX="hidden" position="relative" display = "block">
-          
-          <Box position="relative" width="100%" maxW="2500px" mx="auto">
-
-        <Box
-                position="absolute"
-                right="10%"
-                top="0%"
-                zIndex="2"
-                pointerEvents="none"
-                width={{ base: "100px", md: "196px" }}
-                height={{ base: "50px", md: "100px" }}
-                >
-
-                <Image src={PTop} objectFit="contain" />
-            </Box>
-
-        <Box
-                position="absolute"
-
-                bottom= "0%"
-                left = "30%"
-                zIndex="1"
-                pointerEvents="none"
-                width={{ base: "100px", md: "196px" }}
-                height={{ base: "70px", md: "120px" }}
-                
-                >
-
-                <Image src={PBottom} objectFit="contain"  width={{ base: "100px", md: "196px" }}
-                height={{ base: "70px", md: "120px" }}/>
-            </Box>
-        
-
-
-        <GIC title = "Partnership" description = "We collaborate with healthcare organizations, nonprofits, schools, and funders to expand access to care, strengthen prevention, and build workforce pathways rooted in community trust." buttonName = "Learn More" image_here = {Partners} destination = {"/partners"}></GIC>
-
-
-        
+    <Box width="100%" overflowX="hidden" position="relative" display="block">
+        <Box position="relative" width="100%" maxW="2500px" mx="auto">
+            <GIC
+                title="Partnership"
+                description="We collaborate with healthcare organizations, nonprofits, schools, and funders to expand access to care, strengthen prevention, and build workforce pathways rooted in community trust."
+                buttonName="Learn More"
+                image_here={Partners}
+                destination="/partners"
+                decorations={
+                    <Box
+                        position="absolute"
+                        top="0"
+                        right={{ base: "10%", md: "8%" }}
+                        width={{ base: "140px", md: "180px" }}
+                        height={{ base: "80px", md: "100px" }}
+                        pointerEvents="none"
+                        zIndex={0}
+                    >
+                        <SimpleCircle
+                            size="190px"
+                            strokeWidth="35px"
+                            strokeColor="#5CDAC5"
+                            opacity={0.15}
+                            position="absolute"
+                            left="-40px"
+                            top="-100px"
+                        />
+                    </Box>
+                }
+                imageDecorations={
+                    <Box
+                        position="absolute"
+                        bottom="-95px"
+                        left="300px"
+                        pointerEvents="none"
+                        zIndex={0}
+                    >
+                        <SimpleCircle
+                            size="160px"
+                            strokeWidth="35px"
+                            strokeColor="#5CDAC5"
+                            opacity={0.15}
+                            position="relative"
+                        />
+                    </Box>
+                }
+            />
+        </Box>
     </Box>
-    </Box>
-
     );
 }
--- client/src/components/getInvolved/VolunteerCardGI.jsx
+++ client/src/components/getInvolved/VolunteerCardGI.jsx
@@ -1,54 +1,78 @@
-import { Box, Button, Flex, HStack, Text, VStack, Card, Image } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import SimpleCircle from "../ui/SimpleCircle";
 
 const Volun = "/GetInvolved/GetInvolvedVolunteering.jpg";
-const VTop = "/GetInvolved/vectors/VolunteerTop.svg";
-const VBottom = "/GetInvolved/vectors/VolunteerBottom.svg";
 import GIC from "./GetInvolvedCard"
 
-export default function GetInvolvedCard ({
-
-}) {
+export default function GetInvolvedCard ({}) {
     return (
-    <Box width="100%" overflowX="hidden" position="relative" display = "block">
-          
-          <Box position="relative" width="100%" maxW="2500px" mx="auto">
+    <Box width="100%" position="relative" display="block">
+        <Box position="relative" width="100%" maxW="1283px" mx="auto">
 
-        <Box
+            <GIC
+                title="Volunteering"
+                description="Make a real impact in your community. Volunteers support health fairs, care navigation, and outreach efforts across Los Angeles County. Your time helps neighbors access care, build confidence, and feel supported."
+                buttonName="Learn More"
+                image_here={Volun}
+                destination="https://docs.google.com/forms/d/e/1FAIpQLSfHRyVM1116n02eKWHbwKME1WIrRWQbIy2S44Z-8Ap0V57hYA/viewform"
+                imageDecorations={
+                    <Box
+                        position="absolute"
+                        top="-90px"
+                        left="250px"
+                        pointerEvents="none"
+                        zIndex={0}
+                    >
+                        <SimpleCircle
+                            size="250px"
+                            strokeWidth="25px"
+                            strokeColor="#1573CF"
+                            opacity={0.2}
+                            position="relative"
+                        />
+                    </Box>
+                }
+                decorations={
+                    <Box
+                        position="absolute"
+                        bottom="0"
+                        right={{ base: "10%", md: "35%" }}
+                        width={{ base: "80px", md: "120px" }}
+                        height={{ base: "40px", md: "60px" }}
+                        pointerEvents="none"
+                        zIndex={0}
+                    >
+                        <SimpleCircle
+                            size="200px"
+                            strokeWidth="35px"
+                            strokeColor="#5CDAC5"
+                            opacity={0.15}
+                            position="absolute"
+                            left="-20px"
+                            top="-20px"
+                        />
+                    </Box>
+                }
+            />
+
+            <Box
                 position="absolute"
-                left = "30%"
-                zIndex="2"
+                right="-70px"
+                top="50%"
+                transform="translateY(-50%)"
                 pointerEvents="none"
-                width={{ base: "100px", md: "209px" }}
-                height={{ base: "88px", md: "170px" }}
-                >
-
-                <Image src={VTop} objectFit="contain" />
+                zIndex={0}
+            >
+                <SimpleCircle
+                    size="140px"
+                    strokeWidth="25px"
+                    strokeColor="#5CDAC5"
+                    opacity={0.15}
+                    position="relative"
+                />
             </Box>
 
-        <Box
-                position="absolute"
-                right = "35%"
-                bottom= "0.3%"
-                zIndex="2"
-                pointerEvents="none"
-                height={{ base: "50px", md: "100px" }}
-                width={{ base: "100px", md: "196px" }}
-                
-                >
-
-                <Image src={VBottom} objectFit="contain" height={{ base: "50px", md: "100px" }}
-                width={{ base: "100px", md: "196px" }}/>
-            </Box>
-        
-
-
-        <GIC title = "Volunteering" description = "Make a real impact in your community. Volunteers support health fairs, care navigation, and outreach efforts across Los Angeles County. Your time helps neighbors access care, build confidence, and feel supported." 
-        buttonName = "Learn More" image_here = {Volun} destination={"https://docs.google.com/forms/d/e/1FAIpQLSfHRyVM1116n02eKWHbwKME1WIrRWQbIy2S44Z-8Ap0V57hYA/viewform"}></GIC>
-
-
-        
+        </Box>
     </Box>
-    </Box>
-
     );
 }
